eedecc536a15ff6420d0e1ca66b4ad866255696e
renansilva42 <renansilvageografia@gmail.com>
2025-02-18 14:42:20 -0300

    agora vai

--- get_details_of_deal.py
+++ get_details_of_deal.py
@@ -6,8 +6,8 @@ api_token = os.getenv('PIPEDRIVE_API_TOKEN')
 company_domain = os.getenv('PIPEDRIVE_COMPANY_DOMAIN')
 
 def get_deal_details(deal_id):
-    """Busca todos os detalhes de um deal específico, incluindo pessoa, organização e criador."""
-    url = f'https://{company_domain}.pipedrive.com/api/v1/deals/{deal_id}?api_token={api_token}'
+    """Busca todos os detalhes do deal (deal, pessoa, organização, criador, etc.) em uma única requisição."""
+    url = f'https://{company_domain}.pipedrive.com/api/v1/deals/{deal_id}?api_token={api_token}&include_persons=1&include_organizations=1'
 
     try:
         response = requests.get(url)
@@ -19,38 +19,13 @@ def get_deal_details(deal_id):
             return None
         else:
             deal_data = result['data']
-
-            # Dados da pessoa (person_id)
-            person_data = {}
-            if 'person_id' in deal_data:
-                person_url = f'https://{company_domain}.pipedrive.com/api/v1/persons/{deal_data["person_id"]}?api_token={api_token}'
-                person_response = requests.get(person_url)
-                person_data = person_response.json().get('data', {})
-
-            # Dados da organização (org_id)
-            org_data = {}
-            if 'org_id' in deal_data:
-                org_url = f'https://{company_domain}.pipedrive.com/api/v1/organizations/{deal_data["org_id"]}?api_token={api_token}'
-                org_response = requests.get(org_url)
-                org_data = org_response.json().get('data', {})
-
-            # Dados do criador do deal (creator_user_id)
-            creator_data = {}
-            if 'creator_user_id' in deal_data:
-                creator_url = f'https://{company_domain}.pipedrive.com/api/v1/users/{deal_data["creator_user_id"]}?api_token={api_token}'
-                creator_response = requests.get(creator_url)
-                creator_data = creator_response.json().get('data', {})
-
-            # Combina todos os dados em um único objeto
             full_deal_data = {
                 "deal": deal_data,
-                "person": person_data,
-                "organization": org_data,
-                "creator_user": creator_data
+                "person": deal_data.get('person', {}),
+                "organization": deal_data.get('organization', {}),
+                "creator_user": deal_data.get('creator_user', {})
             }
-
             return full_deal_data
-
     except requests.exceptions.RequestException as e:
         print(f"Erro na requisição para o deal {deal_id}: {str(e)}")
         return None
