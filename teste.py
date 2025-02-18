e5c4d74490885e6aab98fd6ff443d258fecb1d17
renansilva42 <renansilvageografia@gmail.com>
2025-02-18 17:33:17 -0300

    testando apenas teste.py v2

--- teste.py
+++ teste.py
@@ -48,3 +48,4 @@ except json.JSONDecodeError:
     print('Erro ao decodificar a resposta JSON')
 except Exception as e:
     print(f'Ocorreu um erro inesperado: {e}')
+    # vamos ver se agora funciona
